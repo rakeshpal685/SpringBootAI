a34332744e48b7b4b0786b51e5188ec9052a2340
rakeshpal685 <61014188+rakeshpal685@users.noreply.github.com>
2025-06-12 15:53:14 +0800

    Create CustomEnumValidationAnnotation.java

---
+++ product-service/src/main/java/com/rakesh/product_service/exception/customAnnotationForDTO/CustomEnumValidationAnnotation.java
@@ -0,0 +1,15 @@
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = EnumSubsetValidator.class)
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EnumSubset {
+    ProductStatus[] anyOf(); // Allowed values
+    String message() default "must be one of {anyOf}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
